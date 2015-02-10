b31921b4a6e85374c9c67484aeb7ede94a582f04
Daniel <daniel@6temes.cat>
2015-02-10 16:03:56 +0900

    Just a backup

--- code.js
+++ code.js
@@ -1,111 +1,97 @@
 // http://play.elevatorsaga.com/#challenge=5
 
-game = {
+aa = {
     init: function (elevators, floors) {
 
         var maxFloor = 4;
 
-        elevators.forEach(manageElevator);
 
+        function getClosestFloor(currentFloor, queue) {
+            var minDistance,
+                best;
+
+            queue.forEach(function (floor) {
+                var distance = Math.abs(floor - currentFloor);
+                if (!minDistance || distance < minDistance) {
+                    best = floor;
+                    minDistance = distance;
+                }
+            });
+            return best;
+        }
+
+        function findNearestElevator(floorNum) {
+            var minDistance,
+                best;
+
+            elevators.forEach(function (elevator) {
+                var distance = Math.abs(floorNum - elevator.currentFloor());
+                if (!minDistance || distance < minDistance) {
+                    best = elevator;
+                    minDistance = distance;
+                }
+            });
+            return best;
+        }
+
+        floors.forEach(function (floor) {
+            var floorNum = floor.floorNum();
+
+            floor.on("up_button_pressed", function () {
+                var designated = findNearestElevator(floorNum);
+                var queue = designated.destinationQueue || [];
+                designated.destinationQueue.push(floorNum);
+                designated.checkDestinationQueue();
+            });
+            floor.on("down_button_pressed", function () {
+                var designated = findNearestElevator(floorNum);
+                var queue = designated.destinationQueue || [];
+                designated.destinationQueue.push(floorNum);
+                designated.checkDestinationQueue();
+            });
+        });
+
+        elevators.forEach(manageElevator);
         function manageElevator(elevator) {
 
-            elevator._dir = "";
             elevator.direction = function (direction) {
                 if (direction === "up") {
-                    this.goingUpIndicator(true);
-                    this.goingDownIndicator(false);
-                    this._dir = "up";
+                    elevator.goingUpIndicator(true);
+                    elevator.goingDownIndicator(false);
                 } else if (direction === "down") {
-                    this.goingUpIndicator(false);
-                    this.goingDownIndicator(true);
-                    this._dir = "down";
-                } else if (direction === "stop") {
-                    this.goingUpIndicator(true);
-                    this.goingDownIndicator(true);
-                    this._dir = "";
-                }
-                return this._dir;
-            };
-            elevator.hasPassengers = function () {
-                return elevator.getPressedFloors().length > 0;
-            };
-            elevator.getClosestFloor = function (queue) {
-                var minDistance,
-                    best,
-                    currentFloor = this.currentFloor();
-
-                queue.forEach(function (floor) {
-                    var distance = Math.abs(floor - currentFloor);
-                    if (!minDistance || distance < minDistance) {
-                        best = floor;
-                        minDistance = distance;
-                    }
-                });
-
-                return best;
-            };
-
-            elevator.roam = function () {
-                var nextFloor = this.currentFloor();
-
-                if (this.direction() !== "up" && this.direction() !== "down") {
-                    if (this.currentFloor() < maxFloor) {
-                        this.direction("up");
-                    } else {
-                        this.direction("down");
-                    }
+                    elevator.goingUpIndicator(false);
+                    elevator.goingDownIndicator(true);
+                } else if (direction === "any") {
+                    elevator.goingUpIndicator(true);
+                    elevator.goingDownIndicator(true);
+                } else if (direction) {
+                    elevator.goingUpIndicator(false);
+                    elevator.goingDownIndicator(false);
                 }
 
-                if (this.direction() === "up") {
-                    nextFloor = this.currentFloor() + 1;
-                } else if (this.direction() === "down") {
-                    nextFloor = this.currentFloor() - 1;
-                }
-
-                this.goToFloor(nextFloor);
-
-                if (nextFloor === 0) {
-                    this.direction("up");
-                } else if (nextFloor === maxFloor) {
-                    this.direction("down")
+                if (elevator.goingUpIndicator() && !elevator.goingDownIndicator()) {
+                    return "up";
+                } else if (!elevator.goingUpIndicator() && elevator.goingDownIndicator()) {
+                    return "down";
+                } else if (elevator.goingUpIndicator() && elevator.goingDownIndicator()) {
+                    return "any";
                 }
             };
-            elevator.moveToClosestDestination = function () {
-                var closestDestination = this.getClosestFloor(this.getPressedFloors());
 
-                this.direction("stop");
-
-                this.goToFloor(closestDestination);
-            };
-
-            elevator.goToFloor(maxFloor);
             elevator.on("idle", function () {
-                if (this.hasPassengers()) {
-                    this.moveToClosestDestination();
+                console.log("hola");
+
+                if (elevator.getPressedFloors().length > 0) {
+                    var nextFloor = getClosestFloor(elevator.currentFloor(), elevator.getPressedFloors());
+
+                    designated.destinationQueue.push(nextFloor);
+                    designated.checkDestinationQueue();
                 }
-                this.roam();
-            })
+            });
+
         }
     },
     update: function (dt, elevators, floors) {
         // We normally don't need to do anything here
     }
-};
-
-/*
- function zeroToNToOne(n) {
- var i,
- r,
- res = [];
-
- for (i = 0; i <= (n * 2) - 1; i++) {
- if (i <= n) {
- r = i;
- } else {
- r = n - (i % n);
- }
- res.push(r);
- }
- return res;
- }
- */
+}
